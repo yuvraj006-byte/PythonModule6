aedd793303c01439f799c9098481aa5dea9b5877
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-01-29 11:30:03 +0200

    Python Module 6

--- Task6/task6.3.py
+++ Task6/task6.3.py
@@ -1,11 +1,16 @@
-american_gas = int(input("Enter the American Gasoline Amount in gallons: "))
-
 def gal_lit():
+   if american_gas > 0:
+       lit = american_gas * 3.78541
+       print(lit)
+   return
+
+while True:
+    american_gas = int(input("Enter the American Gasoline Amount in gallons: "))
     if american_gas < 0:
         print("Gas can Not be in Negative!")
-        breakpoint()
-    lit = american_gas * 3.78541
-    return print(lit)
-
+        break
+    else:
+        lit = american_gas * 3.78541
+        print(lit)
 gal_lit()
 
