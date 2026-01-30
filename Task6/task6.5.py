5ecfabb019bce1f4491f9a4099e4feed5a1bc5e0
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-01-30 09:51:08 +0200

    Did some Changes after Friday's Q/A seession!

--- Task6/task6.5.py
+++ Task6/task6.5.py
@@ -1,12 +1,9 @@
+user_input = input("Enter a list of numbers (SEPARATED WITH A SINGLE SPACE): ")
+user_list = list(map(int, user_input.split()))
+even = [num for num in user_list if num % 2 == 0]
 def main():
-    user_input = input("Enter a list of numbers (SEPARATED WITH A SINGLE SPACE): ")
-    user_list = list(map(int, user_input.split()))
-
-    even = [num for num in user_list if num % 2 == 0]
-
-    if even:
-        print("The list is: ", even)
-    else:
-        print(None)
-    return
-main()
+    original = "The original List: ", user_list
+    modified = "The modified List: ", even
+    return original, modified
+func_print = main()
+print(func_print)
