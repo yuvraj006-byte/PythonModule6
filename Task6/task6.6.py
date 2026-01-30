4dad49388c27d75ada0dc0c88b710308d8806606
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-01-30 09:35:31 +0200

    Did some Changes after Friday's Q/A seession!

--- Task6/task6.6.py
+++ Task6/task6.6.py
@@ -1,17 +1,26 @@
 import math
+def area_func_one():
+    diameter1 = float(input("Enter the First diameter: "))
+    radius1 = (diameter1 / 2) / 100
+    area1 = math.pi * radius1 ** 2
+    return area1
 
-def main():
-    diameter1 = int(input("Enter the diameter: "))
-    diameter2 = int(input("Enter the diameter: "))
-    radius1 = diameter1 / 2
-    radius2 = diameter2 / 2
-    area1 = math.pi * radius1** 2
+def area_func_two():
+    diameter2 = float(input("Enter the Second diameter: "))
+    radius2 = (diameter2 / 2) / 100
     area2 = math.pi * radius2 ** 2
-    price1 = int(input("Enter the first price: "))
-    price2 = int(input("Enter the second price: "))
+    return area2
 
-    unit_price1 = (area1 / price1)
-    unit_price2 = (area2 / price2)
+
+def price(a,b):
+    return a, b
+price1 = int(input("Enter the first price: "))
+price2 = int(input("Enter the second price: "))
+
+unit_price1 = area_func_one()/price1
+unit_price2 = area_func_one()/price2
+def main():
+    price(unit_price1, unit_price2)
 
     if unit_price1 > unit_price2:
         print("The price of first pizza is better value for money!")
