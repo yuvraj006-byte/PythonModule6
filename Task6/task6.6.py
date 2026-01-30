78a85dd2c1dee5cd48b0793fb0444b825c6bdabb
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-01-30 09:09:26 +0200

    Python Module 6

--- Task6/task6.6.py
+++ Task6/task6.6.py
@@ -1,14 +1,17 @@
 import math
 
 def main():
-    diameter = int(input("Enter the diameter: "))
-    radius = diameter / 2
-    area = math.pi * radius ** 2
+    diameter1 = int(input("Enter the diameter: "))
+    diameter2 = int(input("Enter the diameter: "))
+    radius1 = diameter1 / 2
+    radius2 = diameter2 / 2
+    area1 = math.pi * radius1** 2
+    area2 = math.pi * radius2 ** 2
     price1 = int(input("Enter the first price: "))
     price2 = int(input("Enter the second price: "))
 
-    unit_price1 = (area / price1)
-    unit_price2 = (area / price2)
+    unit_price1 = (area1 / price1)
+    unit_price2 = (area2 / price2)
 
     if unit_price1 > unit_price2:
         print("The price of first pizza is better value for money!")
